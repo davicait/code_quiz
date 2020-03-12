31a416ec31d06034ae95ef10ccb76450290c4808
Caitlynn Davis <caitlynn.davis2@gmail.com>
2020-03-11 20:44:27 -0600

    restarted project

--- questions.js
+++ questions.js
@@ -1,202 +1,24 @@
 // Questions Array
-var ofQuestions = [
+var questions = [
     {
-    question : "What does HTML stand for?",
-    choiceA : "Hyper Text Markup Language",
-    choiceB : "Hyperlinks and Text Markup Languages",
-    choiceC : "Home Tool Markup Language",
-    choiceD : "Hyperlinks That Make Lanuage",
-    correct : "Hyper Text Markup Language",
+    title : "What does HTML stand for?",
+    choices: ["Hyper Text Markup Language", "Hyperlinks and Text Markup Languages", "Home Tool Markup Language", "Hyperlinks That Make Lanuage", "Hyper Text Markup Language"],
+    answer: "Hyper Text Markup Language"
     },{
-    question : "What does DOM stand for?",
-    choiceA : "Documentary Of Modes",
-    choiceB : "Document Object Model",
-    choiceC : "Decision Object Model",
-    choiceD : "Document Oscar Model",
-    correct : "Document Object Model",
+    title : "What does DOM stand for?",
+    choices: ["Documentary Of Modes", "Document Object Model", "Decision Object Model", "Document Oscar Model"],
+    answer : "Document Object Model",
     },{
-    question : "What does the getElementById function do?",
-    choiceA : "Allows you to create an element",
-    choiceB : "Allows you to replace/update the contents of an element",
-    choiceC : "Allows you to select and use a DOM element with the specified ID",
-    choiceD : "getElementById is not a real function",
-    correct : "Allows you to select and use a DOM element with the specified ID",
+    title: "What does the getElementById function do?",
+    choices: ["Allows you to create an element", "Allows you to replace/update the contents of an element", "Allows you to select and use a DOM element with the specified ID", "getElementById is not a real function"],
+    answer : "Allows you to select and use a DOM element with the specified ID",
     },{
-    question : "How do you print something in Javascript?",
-    choiceA : "console. log();",
-    choiceB : "document. write();",
-    choiceC : "alert();",
-    choiceD : "All of the above",
-    correct : "All of the above",
+    title : "How do you print something in Javascript?",
+    choices: ["console. log();", "document. write();", "alert();", "All of the above"],
+    answer : "All of the above",
     },{
-    question : "Which of these is not a Javascript operator?",
-    choiceA : "+",
-    choiceB : "//",
-    choiceC : "!==",
-    choiceD : "&&",
-    correct : "//",
-    },
+    title : "Which of these is not a Javascript operator?",
+    choices: ["+", "//", "!==", "&&"],
+    answer : "//",
+    }
 ];
-
-// Select all elements to update the inner HTML
-
-var start = document.getElementById("start");
-var quiz = document.getElementById("quiz");
-var viewHighScores = document.getElementById("viewHighScores");
-var timer = document.getElementById("timer");
-var questions = document.getElementById("question");
-var result = document.getElementById("result");
-var scores = document.getElementById("scores");
-var result = document.getElementById("result")
-
-var choiceA = document.getElementById("choiceA");
-var choiceB = document.getElementById("choiceB");
-var choiceC = document.getElementById("choiceC");
-var choiceD = document.getElementById("choiceD");
-
-// Create Variables
-
-let lastQuestion = questions.length -1;
-//let runningQuestion = 0;
-
-// Render Question
-
-function renderQuestion(runningQuestion) {
-    console.log(runningQuestion)
-    let q = ofQuestions[runningQuestion];
-    //console.log("q", q);
-    questions.innerHTML = "<p>" + q.question + "</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
-
-    choiceA.addEventListener("click", function(){
-        console.log(this.textContent)
-        runningQuestion++
-        renderQuestion(runningQuestion)
-        if(this.textContent === q.correct){
-           result.innerHTML = "Correct!"
-        }else{
-            result.innerHTML = "Wrong!"
-        }
-
-    })
-
-    choiceB.addEventListener("click", function(){
-        console.log(this.textContent)
-        runningQuestion++
-        renderQuestion(runningQuestion)
-        if(this.textContent === q.correct){
-            result.innerHTML = "Correct!"
-        }else{
-           result.innerHTML = "Wrong!"
-        }
-        
-
-    })
-
-    choiceC.addEventListener("click", function(){
-        console.log(this.textContent)
-        runningQuestion++
-        renderQuestion(runningQuestion)
-        if(this.textContent === q.correct){
-            result.innerHTML = "Correct!";
-        }else{
-           result.innerHTML = "Wrong!";
-        }
-       
-
-    })
-    
-    choiceD.addEventListener("click", function(){
-        console.log(this.textContent)
-        runningQuestion++
-        renderQuestion(runningQuestion)
-        if(this.textContent === q.correct){
-            console.log()
-            result.innerHTML = "Correct!";       
-        }else{
-            result.innerHTML = "Wrong!"   
-        }
-    })
-    
-    
-}
-
-
-
-
-// Display correct answer at the bottom
-// if (answer === ofQuestions[runningQuestion.correct]){
-//     getElementById("score", "Correct!");
-// }else{
-//     getElementById("score", "Wrong!");
-// }
-
-var playing = true;
-start.addEventListener("click", startQuiz);
-
-for(var i=1;i<ofQuestions.length;i++){
-    renderQuestion(i)
-  console.log(list[i]); 
-  if (list[i] === '') {break;}
-}
-
-
-// Start Quiz
-function startQuiz(){
-    start.style.display = "none";
-    renderQuestion(0);
-    quiz.style.display = "block";
-    // timerRender();
-}
-
-//Check User Answer
-
-
-
-// Timer 
-
-var timerCount = 75;
-function timerRender() {
-    // setInterval(countdown, 1000);
-}
-
-
-// function countdown() {
-//     timerCount--;
-//      timer.textContent = timerCount;
-//     if (timerCount === 0){
-//         scoreRender();
-//     }else{
-//         // renderQuestion();
-//     }
-// }
-
-
-// function checkAnswer(){
-//     if( answer == ofQuestions[runningQuestion].correct){
-//         // answer is correct
-//         timerCount+10;
-//         runningQuestion++;
-//         renderQuestion();
-        
-//     }else{
-//         // answer is wrong
-//         timerCount-10;
-//         runningQuestion++;
-//         renderQuestion();
-//     }
-//     if(runningQuestion < lastQuestion){
-//         runningQuestion++;
-//         renderQuestion();
-//     }else{
-//         // end the quiz and show the score
-//         scoreRender();
-//         function scoreRender(){
-//             scoresDiv.style.display = "block";
-//     }
-// }
-// }
-
---
+++ script.js
@@ -0,0 +1,176 @@
+//Start button
+var start = document.getElementById("start-quiz");
+start.addEventListener("click", function(){
+  start.parentNode.removeChild(start);
+  runQuiz();
+})
+
+//variables
+var score = 0;
+var currentQuestion = 0;
+var questionArray = questions;
+var quiz = document.getElementById('quiz');
+var questionDiv = document.getElementById("question");
+var questionDisplay = document.createElement('h2');
+var choicesDiv = document.getElementById("choices");
+var feedback = document.getElementById('feedback');
+var highscores = document.createElement('h2');
+var scoreForm = document.getElementById('hidden');
+scoreForm.style.display = 'none';
+var yourScore = document.querySelector(".yourScore");
+var submitBtn = document.getElementById('submit');
+var main = document.getElementById('main-container');
+restartBtn = document.createElement('button');
+quiz.appendChild(restartBtn);
+restartBtn.style.display = 'none';
+
+//timer variables
+var totalSeconds = questionArray.length * 15;
+var secondsElapsed = 0;
+var interval;
+var secondsDisplay = document.getElementById("timer");
+
+
+//highscores variables
+var totalScore = 0;
+
+//Timer
+var startTimer = function() {
+  interval = setInterval(function() {
+    secondsElapsed++;
+    renderTime();
+  }, 1000);
+}
+
+//render time left
+function renderTime(){
+  var secondsLeft = totalSeconds - secondsElapsed;
+  secondsDisplay.textContent = secondsLeft;
+  if (secondsElapsed >= totalSeconds) {
+   secondsDisplay.textContent = 0;
+   stopTimer();
+  }
+  if (secondsElapsed >= totalSeconds && currentQuestion !== questionArray.length) {
+    outofTime();
+   }
+  }
+  
+  //stopping the timer
+    function stopTimer(){
+      clearInterval(interval);
+      currentQuestion = 0;
+    }
+  //function for displaying out of time message and restarting quiz
+  var outofTime = function() {
+    clearLastAnswer();
+    feedback.textContent = "You ran out of time! No points"
+    score = 0;
+    restartBtn.style.display = 'block';
+    restartBtn.textContent = "Restart Quiz?";
+    restartBtn.addEventListener('click', function(){
+    window.location.reload();
+    })
+}
+
+//run the quiz
+function runQuiz(){
+  startTimer();
+  if(currentQuestion === questionArray.length){
+    stopTimer();
+    stopQuiz();
+  }
+  else{
+    //display the question
+    questionDisplay.textContent = renderQuestion();
+    questionDiv.appendChild(questionDisplay);
+    //display the choices
+    for (i in questionArray[currentQuestion]['choices']){
+      var choicesDisplay = document.createElement("button");
+      choicesDisplay.setAttribute("class", "guess");
+      choicesDisplay.textContent = questionArray[currentQuestion]['choices'][i];
+          choicesDiv.appendChild(choicesDisplay);
+    }
+    //grab which on ethe user clicked and check it against the answer
+    var guess = document.querySelectorAll(".guess");
+      var i = 0, length=guess.length;
+      for (i=0;i<length;i++){
+        if (document.addEventListener) {
+          guess[i].addEventListener("click", function() {
+             var userGuess = this.innerText;
+             var answer = questionArray[currentQuestion]['answer'];
+             checkAnswer(userGuess, answer);
+          });
+      } else {
+        return;
+          }
+      }   
+  }
+}  
+//reset the quiz
+function resetQuiz(){
+  quiz.removeChild(restartBtn);
+  feedback.textContent = '';
+  totalSeconds = 75;
+  score = 0;
+  currentQuestion = 0;
+  runQuiz();
+}
+//check the answer
+function checkAnswer(userGuess, answer) {
+  if (userGuess === answer){
+    //if correct, add to the score, increase the currentQuestion index, clear the last answer, run the quiz
+    score++;
+    totalScore = score;
+    currentQuestion++;
+    clearLastAnswer();
+    feedback.textContent = '';
+    runQuiz();
+    
+    
+  }
+  else{
+    //if wrong, decrease score, take away time, feedback is try again, and return function until they get it right
+    score--;
+    totalSeconds-=15;
+    feedback.textContent = "Try Again"
+    return; 
+  }
+}
+
+//clear last answer
+function clearLastAnswer(){
+  questionDisplay.textContent = "";
+  choicesDiv.innerHTML = "";
+}
+//render question
+function renderQuestion(){
+  return questionArray[currentQuestion]['title'];
+}
+//render choices
+function renderChoices(){
+  return questionArray[currentQuestion]['choices'];
+}
+//stop the quiz
+function stopQuiz(){
+  clearLastAnswer();
+  feedback.innerHTML = '';
+  totalSeconds=0;
+  var finalScore = score;
+  main.parentNode.removeChild(main);
+  scoreForm.setAttribute('id', 'show');
+  scoreForm.style.display = 'block';
+  yourScore.textContent = ' ' + finalScore;
+}
+//on submission
+submitBtn.addEventListener("click",function(event) {
+  event.preventDefault();
+  var initialsInput = document.getElementById("initials");
+  var highscores = JSON.parse(localStorage.getItem('allScores')) || [];
+  highscores.push({initials:initialsInput.value, score:yourScore.innerText})
+  localStorage.setItem("allScores", JSON.stringify(highscores));
+  return redirect();
+},false);
+//redirect on submit
+function redirect(){
+  window.location.href = "highscores.html" ;
+}
